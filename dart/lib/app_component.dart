7d0dc36a0ab63ff4350a2e74ee8d87db9818a581
Jacob Dearing <jacob.dearing@gmail.com>
2018-10-23 03:37:44 -0500

    compiles but dies on attempting to dial out
    EXCEPTION: gRPC Error (14, Error connecting: Unsupported operation: SecurityContext alpnSupported getter)
    STACKTRACE:
    package:grpc/src/client/call.dart 71:17            [_terminateWithError]
    package:grpc/src/client/call.dart 66:24            onConnectionError
    package:grpc/src/client/connection.dart 177:9      [_failCall]
    package:grpc/src/client/connection.dart 253:46     call
    dart:sdk_internal                                  forEach]
    package:grpc/src/client/connection.dart 253:26     [_handleConnectionFailure]
    package:stack_trace                                arg
    package:angular/src/core/zone/ng_zone.dart 190:16  arg
    dart:sdk_internal                                  runUnary
    package:angular/src/core/zone/ng_zone.dart 187:18  [_runUnary]
    dart:sdk_internal                                  <fn>.scheduleMicrotask.dart.fn
    package:angular/src/core/zone/ng_zone.dart 163:9   safeMicrotask
    package:stack_trace                                parent.registerCallback.dart.fn
    package:angular/src/core/zone/ng_zone.dart 178:16  parent.run.dart.fn
    dart:sdk_internal                                  run
    package:angular/src/core/zone/ng_zone.dart 175:18  [_run]
    
    dart:sdk_internal                                  catchError
    package:grpc/src/client/connection.dart 158:19     dispatchCall
    package:grpc/src/client/channel.dart 76:17         connection
    package:stack_trace                                arg
    package:angular/src/core/zone/ng_zone.dart 190:16  arg
    dart:sdk_internal                                  runUnary
    package:angular/src/core/zone/ng_zone.dart 187:18  [_runUnary]
    dart:sdk_internal                                  <fn>.scheduleMicrotask.dart.fn
    package:angular/src/core/zone/ng_zone.dart 163:9   safeMicrotask
    package:stack_trace                                parent.registerCallback.dart.fn
    package:angular/src/core/zone/ng_zone.dart 178:16  parent.run.dart.fn
    dart:sdk_internal                                  run
    package:angular/src/core/zone/ng_zone.dart 175:18  [_run]
    
    dart:sdk_internal                                              then
    package:grpc/src/client/channel.dart 74:18                     createCall
    package:grpc/src/client/client.dart 34:20                      $createCall
    package:dart/src/generated/mud.pbgrpc.dart 32:18               login
    package:dart/app_component.dart 62:17                          new
    package:dart/app_component.template.dart 108:22                build
    package:angular/src/core/linker/app_view.dart 240:12           createHostView
    package:angular/src/core/linker/component_factory.dart 111:20  create
    package:angular/src/core/application_ref.dart 70:41            src__runtime__optimizations.unsafeCast.run.dart.fn
    package:angular/src/core/change_detection/host.dart 250:18     runInZone.dart.fn
    package:angular/src/core/zone/ng_zone.dart 178:16              parent.run.dart.fn
    dart:sdk_internal                                              run
    package:angular/src/core/zone/ng_zone.dart 175:18              [_run]
    dart:sdk_internal                                              run
    package:angular/src/core/zone/ng_zone.dart 327:22              run
    package:angular/src/core/application_ref.dart 140:51           runInZone
    package:angular/src/core/change_detection/host.dart 248:5      run
    package:angular/src/core/application_ref.dart 69:23            bootstrap
    package:angular/src/bootstrap/run.dart 188:16                  runApp
    main.dart 5:3                                                  main
    main.dart.bootstrap.js 780:12                                  <fn>

--- dart/lib/app_component.dart
+++ dart/lib/app_component.dart
@@ -1,12 +1,56 @@
 import 'package:angular/angular.dart';
-
+import 'dart:convert';
 import 'src/todo_list/todo_list_component.dart';
+import 'package:grpc/grpc.dart';
 import 'src/generated/mud.pb.dart';
 import 'src/generated/mud.pbenum.dart';
 import 'src/generated/mud.pbgrpc.dart';
 import 'src/generated/mud.pbjson.dart';
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
+
+
+var cert = '''
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEA3uVQ2ljrGMqM0zy5JF9xLsShfaj8rY4kvIqlCciOEzw1OMtx
+E1JPWoVQx29NLMnzETFANTcfEaFwMStVBBSsaQNzb7srmadi2gUhOJDUvOSmBgpT
+Yls6cDQ0izPS/VDznSX6qcuCc4c1/FI9PVprOTGyPHd/A6d35fr+KYZrIa+4J7pt
+gGYt6DoFVHhOOkX1MWhUUZu8J+RRefqEsbH81/tJGU7pkZHEymQTHxPleUEKI+u6
+VHbJecs807dViYhw8gdUvw6jEOYN3bjqALjPJ8744I6nhg9YGi9QNifqYr6337w8
+vY4EqRXnNvAItcXh7ESOp62HuUKCWnJfpeOsBQIDAQABAoIBAQCQppshCGGR1iks
+ERVJ0JKgOkVfSmKosz2OCh/uyHPDcehMMmLwmhMbeLcipk2cSnbu6lpz7pDAHVe0
+4R0sJ8p34CBzGzHBZsqVGI0lq4AUEgLYmqzggLndBVQwHnLHqFhg3Lveo07GcLIN
+TMWnYl2BYYn9CcOo+G6sFoYJJs2xrqQXcseh5fKUnxFDucLoyza6bOz5RbWSaAfs
+keCZN1QB+epfsKJXhF0lRFN4sUcyzlres2lWJs6L3jUUgDao7BwKtt3YiGnSBMjs
+orKh+iBrOEU6lRORya8iw9M8hVsYHId0c/Q5QmiVRaRK08Btx1YXNI7bNFrctqhD
+MUTX4+vBAoGBAP5uKdc3XzeycUCczh9hv/A0eyzZsGOntNlhQpV1e5LEltJhrjVx
+THtWI+5qWqeXu6F3OgUufk+8YJIrXDjmzHf+C/rId4Od1zuVBm4xpQAQX5qVZpvU
++dghnGDZrNWK9SG45U2lKOAzv9LlF0Jf9tKN9aDDgb7mmgoQkLBAsFE1AoGBAOBF
+WRibikKn34V+X9xhWAShBzuSOAZCskBA3gppOupS3uiytcbENCPzeWk6S7rsCgxv
+3PFfWBwEkgUjm6j+wyQ4PUsyJey/ZR4/vjURYrrBvg9Sx+Ugv+DKyum5qmBIs+b1
+ca6UtF4Fc33TjY1Rl0BZUIYigo5UFyyM+UYgKZmRAoGBAJVldmgK+Xwo6Vs5N8cx
+jJStHE6fnHVHumeSNpiwHVl22eC6vqJe3rvlPr/17zlNXT1YVtdID58eS85Ia6R1
+0UX8BGjDYXoFKqavwNDfkbVR5/FLHK8QBYylX7920YeqSnn8qZhIDJJpkI9zOY8d
+7j6AYOR/KCZCHd0fEPaNZXqpAoGAPQhCTeMORkN/HcoddlD7bg8Qc9K/8lw2PCwr
+APRQNzKkPPldZKFCe0IoGU3nDBBuT6Ilo35J8R4BSuq80sm5+2Jes9cK4RnY/9li
+/vgjQSW2fl8NzToTXa5yeBax2hAO/VxwOo13ZFrhai2nSP0aR0XHFynPE8Kop9QZ
+hk78YzECgYAzJUSN8NaY9HGJvvxo4p+qzHeyUI8NFRJmwm7mfM7tzPdOXeOwVAeY
+yHsbOxsWpxGWH0xlU+qYcI/oFGZmdma/GjivHPA4i2KqfRQPJN7dor0rTD7F8e7G
+K5FH5aM7s4zIrMQATgdYIVnpP063ETaiUspyxbmpAdJ56uCHjmAJww==
+-----END RSA PRIVATE KEY-----
+''';
+
+final trustedRoot = utf8.encode(cert);
+final channelCredentials = new ChannelCredentials.secure(certificates: trustedRoot);
+final channelOptions = new ChannelOptions(credentials: channelCredentials);
+final channel = new ClientChannel('localhost', port: 50051, options: channelOptions);
+final client = new GameClient(channel);
+final loginR = new LoginRequest()
+  ..username = "admin"
+  ..password = "password";
+
+//client.login(loginR);
+
 
 @Component(
   selector: 'my-app',
@@ -15,5 +59,14 @@ import 'src/generated/mud.pbjson.dart';
   directives: [TodoListComponent],
 )
 class AppComponent {
-  // Nothing here yet. All logic is in TodoListComponent.
+  var t = client.login(loginR);
 }
+
+
+// final channel = new ClientChannel('localhost',
+//     port: 50051,
+//     options: const ChannelOptions(
+//         credentials: const ChannelCredentials.insecure()));
+// final stub = new GameClient(channel);
+
+
